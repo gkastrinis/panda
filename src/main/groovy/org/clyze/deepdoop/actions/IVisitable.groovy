b69d6f514628eceb789bca7605c063f43cec750e
George Kastrinis <george.kast@gmail.com>
2017-09-15 18:29:57 +0300

    Clean-up PostOrderVisitor

--- src/main/groovy/org/clyze/deepdoop/actions/IVisitable.groovy
+++ src/main/groovy/org/clyze/deepdoop/actions/IVisitable.groovy
@@ -1,6 +1,5 @@
 package org.clyze.deepdoop.actions
 
 interface IVisitable {
-	// Enabling double dispatch
 	def <T> T accept(IVisitor<T> v)
 }
--- src/main/groovy/org/clyze/deepdoop/actions/PostOrderVisitor.groovy
+++ src/main/groovy/org/clyze/deepdoop/actions/PostOrderVisitor.groovy
@@ -19,135 +19,135 @@ class PostOrderVisitor<T> implements IVisitor<T> {
 
 	T visit(Program n) {
 		actor.enter(n)
-		m[n.globalComp] = n.globalComp.accept(this)
-		n.comps.values().each { m[it] = it.accept(this) }
-		return actor.exit(n, m)
+		m[n.globalComp] = n.globalComp.accept(this) as T
+		n.comps.values().each { m[it] = it.accept(this) as T }
+		actor.exit(n, m)
 	}
 
 	T visit(CmdComponent n) {
 		actor.enter(n)
-		n.exports.each { m[it] = it.accept(this) }
-		n.imports.each { m[it] = it.accept(this) }
-		n.declarations.each { m[it] = it.accept(this) }
-		n.rules.each { m[it] = it.accept(this) }
-		return actor.exit(n, m)
+		n.exports.each { m[it] = it.accept(this) as T }
+		n.imports.each { m[it] = it.accept(this) as T }
+		n.declarations.each { m[it] = it.accept(this) as T }
+		n.rules.each { m[it] = it.accept(this) as T }
+		actor.exit(n, m)
 	}
 
 	T visit(Component n) {
 		actor.enter(n)
-		n.declarations.each { m[it] = it.accept(this) }
-		n.constraints.each { m[it] = it.accept(this) }
-		n.rules.each { m[it] = it.accept(this) }
-		return actor.exit(n, m)
+		n.declarations.each { m[it] = it.accept(this) as T }
+		n.constraints.each { m[it] = it.accept(this) as T }
+		n.rules.each { m[it] = it.accept(this) as T }
+		actor.exit(n, m)
 	}
 
 	T visit(Constraint n) {
 		actor.enter(n)
-		m[n.head] = n.head.accept(this)
-		m[n.body] = n.body.accept(this)
-		return actor.exit(n, m)
+		m[n.head] = n.head.accept(this) as T
+		m[n.body] = n.body.accept(this) as T
+		actor.exit(n, m)
 	}
 
 	T visit(Declaration n) {
 		actor.enter(n)
-		m[n.atom] = n.atom.accept(this)
-		n.types.each { m[it] = it.accept(this) }
-		return actor.exit(n, m)
+		m[n.atom] = n.atom.accept(this) as T
+		n.types.each { m[it] = it.accept(this) as T }
+		actor.exit(n, m)
 	}
 
 	T visit(Rule n) {
 		actor.enter(n)
-		m[n.head] = n.head.accept(this)
-		if (n.body) m[n.body] = n.body.accept(this)
-		return actor.exit(n, m)
+		m[n.head] = n.head.accept(this) as T
+		m[n.body] = n.body?.accept(this) as T
+		actor.exit(n, m)
 	}
 
 	T visit(AggregationElement n) {
 		actor.enter(n)
-		m[n.var] = n.var.accept(this)
-		m[n.predicate] = n.predicate.accept(this)
-		m[n.body] = n.body.accept(this)
-		return actor.exit(n, m)
+		m[n.var] = n.var.accept(this) as T
+		m[n.predicate] = n.predicate.accept(this) as T
+		m[n.body] = n.body.accept(this) as T
+		actor.exit(n, m)
 	}
 
 	T visit(ComparisonElement n) {
 		actor.enter(n)
-		m[n.expr] = n.expr.accept(this)
-		return actor.exit(n, m)
+		m[n.expr] = n.expr.accept(this) as T
+		actor.exit(n, m)
 	}
 
 	T visit(GroupElement n) {
 		actor.enter(n)
-		m[n.element] = n.element.accept(this)
-		return actor.exit(n, m)
+		m[n.element] = n.element.accept(this) as T
+		actor.exit(n, m)
 	}
 
 	T visit(LogicalElement n) {
 		actor.enter(n)
-		n.elements.each { m[it] = it.accept(this) }
-		return actor.exit(n, m)
+		n.elements.each { m[it] = it.accept(this) as T }
+		actor.exit(n, m)
 	}
 
 	T visit(NegationElement n) {
 		actor.enter(n)
-		m[n.element] = n.element.accept(this)
-		return actor.exit(n, m)
+		m[n.element] = n.element.accept(this) as T
+		actor.exit(n, m)
 	}
 
 	T visit(Relation n) {
 		actor.enter(n)
-		return actor.exit(n, m)
+		actor.exit(n, m)
 	}
 
 	T visit(Constructor n) {
 		actor.enter(n)
-		n.keyExprs.each { m[it] = it.accept(this) }
-		m[n.valueExpr] = n.valueExpr.accept(this)
-		m[n.type] = n.type.accept(this)
-		return actor.exit(n, m)
+		n.keyExprs.each { m[it] = it.accept(this) as T }
+		m[n.valueExpr] = n.valueExpr.accept(this) as T
+		m[n.type] = n.type.accept(this) as T
+		actor.exit(n, m)
 	}
 
 	T visit(Type n) {
 		actor.enter(n)
-		n.exprs.each { m[it] = it.accept(this) }
-		return actor.exit(n, m)
+		n.exprs.each { m[it] = it.accept(this) as T }
+		actor.exit(n, m)
 	}
 
 	T visit(Functional n) {
 		actor.enter(n)
-		n.keyExprs.each { m[it] = it.accept(this) }
-		if (n.valueExpr) m[n.valueExpr] = n.valueExpr.accept(this)
-		return actor.exit(n, m)
+		n.keyExprs.each { m[it] = it.accept(this) as T }
+		m[n.valueExpr] = n.valueExpr.accept(this) as T
+		actor.exit(n, m)
 	}
 
 	T visit(Predicate n) {
 		actor.enter(n)
-		n.exprs.each { m[it] = it.accept(this) }
-		return actor.exit(n, m)
+		n.exprs.each { m[it] = it.accept(this) as T }
+		actor.exit(n, m)
 	}
 
 	T visit(Primitive n) {
 		actor.enter(n)
-		m[n.var] = n.var.accept(this)
-		return actor.exit(n, m)
+		m[n.var] = n.var.accept(this) as T
+		actor.exit(n, m)
 	}
 
 	T visit(BinaryExpr n) {
 		actor.enter(n)
-		m[n.left] = n.left.accept(this)
-		m[n.right] = n.right.accept(this)
-		return actor.exit(n, m)
+		m[n.left] = n.left.accept(this) as T
+		m[n.right] = n.right.accept(this) as T
+		actor.exit(n, m)
 	}
 
 	T visit(ConstantExpr n) {
 		actor.enter(n)
-		return actor.exit(n, m)
+		actor.exit(n, m)
 	}
 
 	T visit(GroupExpr n) {
 		actor.enter(n)
-		m[n.expr] = n.expr.accept(this)
-		return actor.exit(n, m)
+		m[n.expr] = n.expr.accept(this) as T
+		actor.exit(n, m)
 	}
 
 	// Handling of RecordExpr is not supported in general since it is reserved for interal use
@@ -156,6 +156,6 @@ class PostOrderVisitor<T> implements IVisitor<T> {
 
 	T visit(VariableExpr n) {
 		actor.enter(n)
-		return actor.exit(n, m)
+		actor.exit(n, m)
 	}
 }
