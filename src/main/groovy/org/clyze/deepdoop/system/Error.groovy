fed340c48939d7bafd3593b462600215fc7b8d17
George Kastrinis <george.kast@gmail.com>
2018-03-26 15:24:06 +0300

    Clean-up code

--- src/main/groovy/org/clyze/deepdoop/system/Error.groovy
+++ src/main/groovy/org/clyze/deepdoop/system/Error.groovy
@@ -45,7 +45,6 @@ enum Error {
 	TYPE_UNKNOWN,
 	TYPE_RULE,
 
-
 //	MULTIPLE_ENT_DECLS,
 //	DEP_CYCLE,
 //	DEP_GLOBAL,
@@ -55,59 +54,56 @@ enum Error {
 //	CMD_NO_DECL,
 //	CMD_NO_IMPORT,
 
-	static Map<Error, String> msgMap
-	static {
-		msgMap = new EnumMap<>(Error.class)
-		msgMap[ANNOTATION_NON_EMPTY] = "Annotation `{0}` takes no arguments"
-		msgMap[ANNOTATION_MISSING_ARG] = "Missing mandatory argument `{0}` for annotation `{1}`"
-		msgMap[ANNOTATION_INVALID_ARG] = "Invalid argument `{0}` for annotation `{1}`"
-		msgMap[ANNOTATION_MISTYPED_ARG] = "Type mismatch ({0} instead of {1}) for argument `{2}` of annotation `{3}`"
-		msgMap[ANNOTATION_MULTIPLE] = "Annotation `{0}` appears multiple times in clause"
-		msgMap[ANNOTATION_INVALID] = "Invalid annotation `{0}` for `{1}`"
+	static Map<Error, String> msgMap = [
+			(ANNOTATION_NON_EMPTY)     : "Annotation `{0}` takes no arguments",
+			(ANNOTATION_MISSING_ARG)   : "Missing mandatory argument `{0}` for annotation `{1}`",
+			(ANNOTATION_INVALID_ARG)   : "Invalid argument `{0}` for annotation `{1}`",
+			(ANNOTATION_MISTYPED_ARG)  : "Type mismatch ({0} instead of {1}) for argument `{2}` of annotation `{3}`",
+			(ANNOTATION_MULTIPLE)      : "Annotation `{0}` appears multiple times in clause",
+			(ANNOTATION_INVALID)       : "Invalid annotation `{0}` for `{1}`",
 
-		msgMap[COMP_UNKNOWN] = "Unknown component name `{0}`"
-		msgMap[COMP_ID_IN_USE] = "Component name `{0}` already in use"
-		msgMap[COMP_DUPLICATE_PARAMS] = "Duplicate parameters ({0}) in component `{1}`"
-		msgMap[COMP_SUPER_PARAM_MISMATCH] = "Super component parameters ({0}) not matching parameters ({1}) of component `{2}`"
-		msgMap[COMP_INST_ARITY] = "Wrong arity for instantiation parameters ({0}) of component `{1}` as `{2}`"
-		msgMap[COMP_UNKNOWN_PARAM] = "Unknown `@` parameter `{0}` used in component"
-		msgMap[INST_ID_IN_USE] = "Instantiation name `{0}` already in use"
+			(COMP_UNKNOWN)             : "Unknown component name `{0}`",
+			(COMP_ID_IN_USE)           : "Component name `{0}` already in use",
+			(COMP_DUPLICATE_PARAMS)    : "Duplicate parameters ({0}) in component `{1}`",
+			(COMP_SUPER_PARAM_MISMATCH): "Super component parameters ({0}) not matching parameters ({1}) of component `{2}`",
+			(COMP_INST_ARITY)          : "Wrong arity for instantiation parameters ({0}) of component `{1}` as `{2}`",
+			(COMP_UNKNOWN_PARAM)       : "Unknown `@` parameter `{0}` used in component",
+			(INST_ID_IN_USE)           : "Instantiation name `{0}` already in use",
 
-		msgMap[DECL_MALFORMED] = "Number of variables and types in declaration not matching"
-		msgMap[DECL_MULTIPLE] = "Multiple declarations for relation or type `{0}`"
-		msgMap[CONSTR_NON_FUNC] = "Constructor `{0}` must use functional syntax"
-		msgMap[FUNC_NON_CONSTR] = "Functional syntax available only for constructors (`{0}`)"
-		msgMap[REL_ARITY] = "Inconsistent arity for relation `{0}`"
-		msgMap[REL_EXT_INVALID] = "Relation with a `@` parameter only allowed in a rule body, inside a component"
-		msgMap[REL_EXT_NO_DECL] = "Unknown relation `{0}` used with a `@` parameter"
-		msgMap[REL_NO_DECL] = "Unknown relation `{0}`"
-		msgMap[CONSTR_RULE_CYCLE] = "Constructor `{0}` appears in a cycle in rule head"
-		msgMap[CONSTR_UNKNOWN] = "Unknown constructor `{0}`"
-		msgMap[CONSTR_TYPE_INCOMP] = "Constructor `{0}` used with incompatible type `{1}`"
-		msgMap[VAR_MULTIPLE_CONSTR] = "Var `{0}` constructed by multiple constructors in rule head"
-		msgMap[VAR_UNKNOWN] = "Unknown var `{0}`"
-		msgMap[VAR_UNUSED] = "Unused var `{0}`"
-		msgMap[VAR_ASGN_CYCLE] = "Assignment on var `{0}` is part of an assignment cycle"
-		msgMap[VAR_ASGN_COMPLEX] = "Assignment on var `{0}` is part of a complicated logical structure"
+			(DECL_MALFORMED)           : "Number of variables and types in declaration not matching",
+			(DECL_MULTIPLE)            : "Multiple declarations for relation or type `{0}`",
+			(CONSTR_NON_FUNC)          : "Constructor `{0}` must use functional syntax",
+			(FUNC_NON_CONSTR)          : "Functional syntax available only for constructors (`{0}`)",
+			(REL_ARITY)                : "Inconsistent arity for relation `{0}`",
+			(REL_EXT_INVALID)          : "Relation with a `@` parameter only allowed in a rule body, inside a component",
+			(REL_EXT_NO_DECL)          : "Unknown relation `{0}` used with a `@` parameter",
+			(REL_NO_DECL)              : "Unknown relation `{0}`",
+			(CONSTR_RULE_CYCLE)        : "Constructor `{0}` appears in a cycle in rule head",
+			(CONSTR_UNKNOWN)           : "Unknown constructor `{0}`",
+			(CONSTR_TYPE_INCOMP)       : "Constructor `{0}` used with incompatible type `{1}`",
+			(VAR_MULTIPLE_CONSTR)      : "Var `{0}` constructed by multiple constructors in rule head",
+			(VAR_UNKNOWN)              : "Unknown var `{0}`",
+			(VAR_UNUSED)               : "Unused var `{0}`",
+			(VAR_ASGN_CYCLE)           : "Assignment on var `{0}` is part of an assignment cycle",
+			(VAR_ASGN_COMPLEX)         : "Assignment on var `{0}` is part of a complicated logical structure",
 
-		msgMap[TYPE_INFERENCE_FAIL] = "Type inference was inconclusive: cannot reach fixpoint"
-		msgMap[TYPE_INFERENCE_FIXED] = "Type inference in conflict with declared type `{0}` (at index {1}) for relation `{2}`"
-		msgMap[TYPE_INCOMP_EXPR] = "Incompatible types for numeric expression"
-		msgMap[TYPE_INCOMP] = "Incompatible types for relation `{0}` (at index {1})"
-		msgMap[TYPE_UNSUPP] = "Currently unsupported type `{0}`"
-		msgMap[TYPE_UNKNOWN] = "Unknown type `{0}`"
-		msgMap[TYPE_RULE] = "Type `{0}` used as a normal relation in rule head"
+			(TYPE_INFERENCE_FAIL)      : "Type inference was inconclusive: cannot reach fixpoint",
+			(TYPE_INFERENCE_FIXED)     : "Type inference in conflict with declared type `{0}` (at index {1}) for relation `{2}`",
+			(TYPE_INCOMP_EXPR)         : "Incompatible types for numeric expression",
+			(TYPE_INCOMP)              : "Incompatible types for relation `{0}` (at index {1})",
+			(TYPE_UNSUPP)              : "Currently unsupported type `{0}`",
+			(TYPE_UNKNOWN)             : "Unknown type `{0}`",
+			(TYPE_RULE)                : "Type `{0}` used as a normal relation in rule head",
 
-
-//		msgMap[MULTIPLE_ENT_DECLS] = "Multiple declarations for Type `{0}` in previous components"
-//		msgMap[DEP_CYCLE] = "Cycle detected in the dependency graph of components"
-//		msgMap[DEP_GLOBAL] = "Reintroducing relation `{0}` to global space"
-//		msgMap[CMD_DIRECTIVE] = "Invalid directive in command block `{0}`"
-//		msgMap[CMD_EVAL] = "EVAL property already specified in command block `{0}`"
-//		msgMap[CMD_RULE] = "Normal rules are not supported in a command block"
-//		msgMap[CMD_NO_DECL] = "Relation `{0}` is imported but has no declaration"
-//		msgMap[CMD_NO_IMPORT] = "Relation `{0}` is declared but not imported"
-	}
+//			(MULTIPLE_ENT_DECLS)       : "Multiple declarations for Type `{0}` in previous components",
+//			(DEP_CYCLE)                : "Cycle detected in the dependency graph of components",
+//			(DEP_GLOBAL)               : "Reintroducing relation `{0}` to global space",
+//			(CMD_DIRECTIVE)            : "Invalid directive in command block `{0}`",
+//			(CMD_EVAL)                 : "EVAL property already specified in command block `{0}`",
+//			(CMD_RULE)                 : "Normal rules are not supported in a command block",
+//			(CMD_NO_DECL)              : "Relation `{0}` is imported but has no declaration",
+//			(CMD_NO_IMPORT)            : "Relation `{0}` is declared but not imported",
+	]
 
 	static void warn(SourceLocation loc = null, Error errorId, Object... values) {
 		def msg = "[DD] WARNING: ${MessageFormat.format(msgMap.get(errorId), values)}"
--- src/test/groovy/org/clyze/deepdoop/DatalogSpec.groovy
+++ src/test/groovy/org/clyze/deepdoop/DatalogSpec.groovy
@@ -20,19 +20,19 @@ class DatalogSpec extends Specification {
 		notThrown(DeepDoopException)
 
 		where:
-		file        | _
-		"t0.logic"  | _
-		"t1.logic"  | _
-		"t2.logic"  | _
-		"t3.logic"  | _
-		"t4.logic"  | _
-		"t5.logic"  | _
-		"t6.logic"  | _
-		"t7.logic"  | _
-		"t8.logic"  | _
-		"t9.logic"  | _
-		"t10.logic" | _
-		//"t100.logic" | _
+		file  | _
+		"t0"  | _
+		"t1"  | _
+		"t2"  | _
+		"t3"  | _
+		"t4"  | _
+		"t5"  | _
+		"t6"  | _
+		"t7"  | _
+		"t8"  | _
+		"t9"  | _
+		"t10" | _
+		//"t100" | _
 	}
 
 	@Unroll
@@ -45,49 +45,49 @@ class DatalogSpec extends Specification {
 		e.error == expectedErrorId
 
 		where:
-		file           | expectedErrorId
-		"fail0.logic"  | Error.COMP_UNKNOWN_PARAM
-		"fail1.logic"  | Error.INST_ID_IN_USE
-		"fail2.logic"  | Error.COMP_ID_IN_USE
-		"fail3.logic"  | Error.COMP_UNKNOWN
-		"fail4.logic"  | Error.INST_ID_IN_USE
-		"fail5.logic"  | Error.REL_EXT_NO_DECL
-		"fail6.logic"  | Error.CONSTR_TYPE_INCOMP
-		"fail7.logic"  | Error.FUNC_NON_CONSTR
-		"fail8.logic"  | Error.TYPE_UNKNOWN
-		"fail9.logic"  | Error.CONSTR_UNKNOWN
-		"fail10.logic" | Error.CONSTR_NON_FUNC
-		"fail11.logic" | Error.TYPE_INCOMP
-		"fail12.logic" | Error.REL_ARITY
-		"fail13.logic" | Error.TYPE_INFERENCE_FIXED
-		"fail14.logic" | Error.TYPE_RULE
-		"fail15.logic" | Error.DECL_MULTIPLE
-		"fail16.logic" | Error.ANNOTATION_NON_EMPTY
-		"fail17.logic" | Error.ANNOTATION_MISSING_ARG
-		"fail18.logic" | Error.ANNOTATION_INVALID_ARG
-		"fail19.logic" | Error.ANNOTATION_INVALID
-		"fail20.logic" | Error.REL_EXT_INVALID
-		"fail21.logic" | Error.DECL_MALFORMED
-		"fail22.logic" | Error.ANNOTATION_MISTYPED_ARG
-		"fail23.logic" | Error.CONSTR_RULE_CYCLE
-		"fail24.logic" | Error.VAR_MULTIPLE_CONSTR
-		"fail25.logic" | Error.TYPE_INCOMP_EXPR
-		"fail26.logic" | Error.COMP_DUPLICATE_PARAMS
-		"fail27.logic" | Error.COMP_SUPER_PARAM_MISMATCH
-		"fail28.logic" | Error.COMP_INST_ARITY
-		"fail29.logic" | Error.REL_EXT_INVALID
-		"fail30.logic" | Error.COMP_UNKNOWN
-		"fail31.logic" | Error.VAR_UNKNOWN
-		"fail32.logic" | Error.TYPE_INFERENCE_FAIL
-		"fail33.logic" | Error.REL_ARITY
-		"fail34.logic" | Error.REL_NO_DECL
-//		"fail100.logic" | Error.DEP_CYCLE
-//		"fail101.logic" | Error.CMD_RULE
-//		"fail103.logic" | Error.CMD_DIRECTIVE
-//		"fail104.logic" | Error.CMD_NO_DECL
-//		"fail105.logic" | Error.CMD_NO_IMPORT
-//		"fail106.logic" | Error.CMD_EVAL
-//		"fail108.logic" | Error.MULTIPLE_ENT_DECLS
+		file     | expectedErrorId
+		"fail0"  | Error.COMP_UNKNOWN_PARAM
+		"fail1"  | Error.INST_ID_IN_USE
+		"fail2"  | Error.COMP_ID_IN_USE
+		"fail3"  | Error.COMP_UNKNOWN
+		"fail4"  | Error.INST_ID_IN_USE
+		"fail5"  | Error.REL_EXT_NO_DECL
+		"fail6"  | Error.CONSTR_TYPE_INCOMP
+		"fail7"  | Error.FUNC_NON_CONSTR
+		"fail8"  | Error.TYPE_UNKNOWN
+		"fail9"  | Error.CONSTR_UNKNOWN
+		"fail10" | Error.CONSTR_NON_FUNC
+		"fail11" | Error.TYPE_INCOMP
+		"fail12" | Error.REL_ARITY
+		"fail13" | Error.TYPE_INFERENCE_FIXED
+		"fail14" | Error.TYPE_RULE
+		"fail15" | Error.DECL_MULTIPLE
+		"fail16" | Error.ANNOTATION_NON_EMPTY
+		"fail17" | Error.ANNOTATION_MISSING_ARG
+		"fail18" | Error.ANNOTATION_INVALID_ARG
+		"fail19" | Error.ANNOTATION_INVALID
+		"fail20" | Error.REL_EXT_INVALID
+		"fail21" | Error.DECL_MALFORMED
+		"fail22" | Error.ANNOTATION_MISTYPED_ARG
+		"fail23" | Error.CONSTR_RULE_CYCLE
+		"fail24" | Error.VAR_MULTIPLE_CONSTR
+		"fail25" | Error.TYPE_INCOMP_EXPR
+		"fail26" | Error.COMP_DUPLICATE_PARAMS
+		"fail27" | Error.COMP_SUPER_PARAM_MISMATCH
+		"fail28" | Error.COMP_INST_ARITY
+		"fail29" | Error.REL_EXT_INVALID
+		"fail30" | Error.COMP_UNKNOWN
+		"fail31" | Error.VAR_UNKNOWN
+		"fail32" | Error.TYPE_INFERENCE_FAIL
+		"fail33" | Error.REL_ARITY
+		"fail34" | Error.REL_NO_DECL
+//		"fail100" | Error.DEP_CYCLE
+//		"fail101" | Error.CMD_RULE
+//		"fail103" | Error.CMD_DIRECTIVE
+//		"fail104" | Error.CMD_NO_DECL
+//		"fail105" | Error.CMD_NO_IMPORT
+//		"fail106" | Error.CMD_EVAL
+//		"fail108" | Error.MULTIPLE_ENT_DECLS
 	}
 
 	@Unroll
@@ -100,20 +100,20 @@ class DatalogSpec extends Specification {
 		e.error == expectedErrorId
 
 		where:
-		file            | expectedErrorId
-		"fail-S0.logic" | Error.VAR_ASGN_CYCLE
-		"fail-S1.logic" | Error.VAR_ASGN_COMPLEX
+		file      | expectedErrorId
+		"fail-S0" | Error.VAR_ASGN_CYCLE
+		"fail-S1" | Error.VAR_ASGN_COMPLEX
 	}
 
 	def lbTest(String file) {
-		def resourcePath = "/$file"
+		def resourcePath = "/${file}.logic"
 		def inputStream = new ANTLRInputStream(this.class.getResourceAsStream(resourcePath))
 		def resource = this.class.getResource(resourcePath).file
 		Compiler.compile0(inputStream, resource, new LBCodeGenerator(new File("build")))
 	}
 
 	def souffleTest(String file) {
-		def resourcePath = "/$file"
+		def resourcePath = "/${file}.logic"
 		def inputStream = new ANTLRInputStream(this.class.getResourceAsStream(resourcePath))
 		def resource = this.class.getResource(resourcePath).file
 		Compiler.compile0(inputStream, resource, new SouffleCodeGenerator(new File("build")))
