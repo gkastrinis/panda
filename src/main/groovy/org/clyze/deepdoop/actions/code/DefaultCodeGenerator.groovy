771573a15cd68d356fb03ee9acb01305c96d56d7
George Kastrinis <george.kast@gmail.com>
2018-04-01 15:08:35 +0300

    Fix grouping of negated elements

--- src/main/groovy/org/clyze/deepdoop/actions/code/DefaultCodeGenerator.groovy
+++ src/main/groovy/org/clyze/deepdoop/actions/code/DefaultCodeGenerator.groovy
@@ -26,7 +26,8 @@ class DefaultCodeGenerator extends DefaultVisitor<String> {
 	private File outDir
 	private File currentFile
 	private FileWriter fw
-	private List<LogicalElement.LogicType> logicTypes = []
+	// Keep track of active logical and negation elements in order to group them correctly
+	private List<Integer> complexElements = []
 
 	DefaultCodeGenerator(File outDir) { this.outDir = outDir }
 
@@ -38,15 +39,20 @@ class DefaultCodeGenerator extends DefaultVisitor<String> {
 
 	String exit(GroupElement n) { "(${m[n.element]})" }
 
-	void enter(LogicalElement n) { logicTypes << n.type }
+	void enter(LogicalElement n) { complexElements << map(n) }
 
 	String exit(LogicalElement n) {
-		logicTypes = logicTypes.dropRight(1)
+		complexElements = complexElements.dropRight(1)
 		def str = n.elements.findAll { m[it] }.collect { m[it] }.join(n.type == AND ? ", " : "; ")
-		(logicTypes && logicTypes.last() != n.type) ? "($str)" : str
+		(complexElements && complexElements.last() != map(n)) ? "($str)" : str
 	}
 
-	String exit(NegationElement n) { "!${m[n.element]}" }
+	void enter(NegationElement n) { complexElements << map(n) }
+
+	String exit(NegationElement n) {
+		complexElements = complexElements.dropRight(1)
+		"!${m[n.element]}"
+	}
 
 	String exit(BinaryExpr n) { "${m[n.left]} ${n.op} ${m[n.right]}" }
 
@@ -65,4 +71,8 @@ class DefaultCodeGenerator extends DefaultVisitor<String> {
 	File getCurrentFile() { currentFile }
 
 	void emit(String data) { fw.write "$data\n" }
+
+	private static int map(LogicalElement n) { n.type == AND ? 0 : 1 }
+
+	private static int map(NegationElement n) { 2 }
 }
