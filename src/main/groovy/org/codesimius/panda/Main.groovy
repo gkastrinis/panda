00203a1e42f7279f2efa8a52af135cebfb27e079
George Kastrinis <george.kast@gmail.com>
2018-04-04 16:45:44 +0300

    Change default outDir for code generation

--- src/main/groovy/org/codesimius/panda/Main.groovy
+++ src/main/groovy/org/codesimius/panda/Main.groovy
@@ -2,5 +2,5 @@ package org.codesimius.panda
 
 import org.codesimius.panda.system.Compiler
 
-println Compiler.compileToLB3(args[0], "build")
-println Compiler.compileToSouffle(args[0], "build")
+println Compiler.compileToLB3(args[0], "build/out")
+println Compiler.compileToSouffle(args[0], "build/out")
--- src/test/groovy/org/codesimius/panda/DatalogSpec.groovy
+++ src/test/groovy/org/codesimius/panda/DatalogSpec.groovy
@@ -112,7 +112,7 @@ class DatalogSpec extends Specification {
 		def resourcePath = "/${file}.pnd"
 		def inputStream = new ANTLRInputStream(this.class.getResourceAsStream(resourcePath))
 		def resource = this.class.getResource(resourcePath).file
-		Compiler.compile0(inputStream, resource, codeGen.newInstance("build"))
+		Compiler.compile0(inputStream, resource, codeGen.newInstance("build/out"))
 	}
 
 	def test(String file) {
