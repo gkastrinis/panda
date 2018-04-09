fed87343a5e4e0103a2baacc24c16ad047bedfe4
George Kastrinis <george.kast@gmail.com>
2018-04-09 23:08:31 +0300

    Clean-up code in DOTGenerator

--- src/main/groovy/org/codesimius/panda/system/DOTGenerator.groovy
+++ src/main/groovy/org/codesimius/panda/system/DOTGenerator.groovy
@@ -14,61 +14,67 @@ class DOTGenerator {
 
 	File outDir
 	DependencyGraphVisitor dependencyGraph
-	private FileWriter fw
 
 	void gen() {
+		def FONT = "Arial"
+		def REL_NODE_COLOR = "#cce5ff"
+		def COMP_COLOR = "#c4c4c4"
+		def PARAM_NODE_COLOR = "gold"
+		def INST_NODE_COLOR = "orange"
+		def INHERIT_FONT_COLOR = "#666666"
+		def PARAM_FONT_COLOR = INHERIT_FONT_COLOR
+		def INST_FONT_COLOR = INHERIT_FONT_COLOR
+		def NEG_FONT_COLOR = "red"
+		def DEF_FONT_COLOR = "#333333"
+
 		def f = Files.createTempFile(Paths.get(outDir.path), "graph_", ".dot").toFile()
-		fw = new FileWriter(f)
+		def fw = new FileWriter(f)
+		def emit = { fw.write "$it\n" }
 
 		def log = LogFactory.getLog(DOTGenerator.class)
 		log.info("[DD] GRAPH: ${f.canonicalPath}")
 
-		emit "digraph G {"
-		emit "\trankdir=LR;"
-		emit "\toutputorder=edgesfirst;"
-		emit """\tnode [style=filled,fillcolor="#cce5ff",fontname="Arial"];"""
-		emit """\tedge [fontname="Arial"];"""
+		emit "digraph {"
+		emit "rankdir=LR;"
+		emit "outputorder=edgesfirst;"
+		emit """node [style=filled,fillcolor="$REL_NODE_COLOR",fontname="$FONT"];"""
+		emit """edge [fontname="$FONT"];"""
 		dependencyGraph.graphs.each { graphName, graph ->
 			if (graphName != "_") {
-				emit "\tsubgraph cluster_$graphName {"
-				emit """\t\tlabel="$graphName";style=rounded;bgcolor="#c4c4c4";peripheries=0;fontname="Arial";"""
+				emit "subgraph cluster_$graphName {"
+				emit """label="$graphName";style=rounded;bgcolor="$COMP_COLOR";peripheries=0;fontname="$FONT";"""
 			}
 			graph.nodes.each { nodeId, node ->
 				if (nodeId == node.name)
-					emit """\t\t"$nodeId" [label="",shape=circle,fillcolor=black,width=0.15];"""
+					emit """"$nodeId" [label="",shape=circle,fillcolor=black,width=0.15];"""
 				else if (node.kind == Node.Kind.PARAMETER)
-					emit """\t\t"$nodeId" [label="${node.name}",shape=diamond,fillcolor="gold"];"""
+					emit """"$nodeId" [label="${node.name}",shape=diamond,fillcolor="$PARAM_NODE_COLOR"];"""
 				else if (node.kind == Node.Kind.INSTANCE)
-					emit """\t\t"$nodeId" [label="${node.name}",shape=octagon,fillcolor="orange"];"""
+					emit """"$nodeId" [label="${node.name}",shape=octagon,fillcolor="$INST_NODE_COLOR"];"""
 				else
-					emit """\t\t"$nodeId" [label="${node.name}",shape=circle];"""
+					emit """"$nodeId" [label="${node.name}",shape=circle];"""
 
 				node.outEdges.each {
+					def toId = it.node.id
+					def l = it.label
+
 					if (it.kind == Edge.Kind.INHERITANCE)
-						emit """\t\t"$nodeId" -> "${it.node.id}" [label="${
-							it.label
-						}",fontcolor="#666666",style=dashed,color="#666666"];"""
+						emit """"$nodeId" -> "$toId" [label="$l",fontcolor="$INHERIT_FONT_COLOR",color="$INHERIT_FONT_COLOR",style=dashed];"""
 					else if (it.kind == Edge.Kind.PARAMETER && node.kind == Node.Kind.PARAMETER)
-						emit """\t\t"$nodeId" -> "${it.node.id}" [label="${
-							it.label
-						}",fontcolor="#666666",color="#666666:gold",penwidth=2,arrowhead=none];"""
+						emit """"$nodeId" -> "$toId" [label="$l",fontcolor="$PARAM_FONT_COLOR",color="$PARAM_FONT_COLOR:$PARAM_NODE_COLOR",penwidth=2,arrowhead=none];"""
 					else if (it.kind == Edge.Kind.PARAMETER)
-						emit """\t\t"$nodeId" -> "${it.node.id}" [label="${
-							it.label
-						}",fontcolor="#666666",color="#666666:orange",penwidth=2];"""
+						emit """"$nodeId" -> "$toId" [label="$l",fontcolor="$PARAM_FONT_COLOR",color="$PARAM_FONT_COLOR:$INST_NODE_COLOR",penwidth=2];"""
 					else if (it.kind == Edge.Kind.INSTANCE)
-						emit """\t\t"$nodeId" -> "${it.node.id}" [color="#666666",arrowhead=none];"""
+						emit """"$nodeId" -> "$toId" [color="$INST_FONT_COLOR",arrowhead=none];"""
 					else if (it.kind == Edge.Kind.NEGATED)
-						emit """\t\t"$nodeId" -> "${it.node.id}" [color="red"];"""
+						emit """"$nodeId" -> "$toId" [color="$NEG_FONT_COLOR"];"""
 					else
-						emit """\t\t"$nodeId" -> "${it.node.id}" [color="#333333"];"""
+						emit """"$nodeId" -> "$toId" [color="$DEF_FONT_COLOR"];"""
 				}
 			}
-			if (graphName != "_") emit "\t};"
+			if (graphName != "_") emit "}"
 		}
 		emit "}"
 		fw.close()
 	}
-
-	void emit(String data) { fw.write "$data\n" }
 }
