f84cee1c3d50395a75c40192954e1e453fe5fa91
George Kastrinis <george.kast@gmail.com>
2018-04-25 18:25:31 +0300

    Fix minor issue re input transformations

--- src/main/groovy/org/codesimius/panda/actions/tranform/InputFactsTransformer.groovy
+++ src/main/groovy/org/codesimius/panda/actions/tranform/InputFactsTransformer.groovy
@@ -17,6 +17,7 @@ import org.codesimius.panda.datalog.element.relation.Type
 import org.codesimius.panda.datalog.expr.ConstantExpr
 
 import static org.codesimius.panda.datalog.Annotation.INPUT
+import static org.codesimius.panda.datalog.Annotation.TYPE
 import static org.codesimius.panda.datalog.element.relation.Type.TYPE_STRING
 import static org.codesimius.panda.datalog.expr.VariableExpr.gen1 as var1
 import static org.codesimius.panda.datalog.expr.VariableExpr.genN as varN
@@ -34,7 +35,7 @@ class InputFactsTransformer extends DefaultTransformer {
 
 	IVisitable exit(RelDeclaration n) {
 		if (INPUT in n.annotations) {
-			genInput(n.relation.name, n.types, false)
+			genInput(n.relation.name, n.types, n.annotations)
 			return null
 		}
 		return n
@@ -42,11 +43,11 @@ class InputFactsTransformer extends DefaultTransformer {
 
 	IVisitable exit(TypeDeclaration n) {
 		if (INPUT in n.annotations)
-			genInput(n.type.name, [n.type], true)
+			genInput(n.type.name, [n.type], n.annotations)
 		return n
 	}
 
-	def genInput(String name, List<Type> types, boolean inTypeDecl) {
+	def genInput(String name, List<Type> types, Set<Annotation> annotations) {
 		def N = types.size()
 		def headElements = []
 		def vars = []
@@ -64,12 +65,13 @@ class InputFactsTransformer extends DefaultTransformer {
 			}
 		}
 
+		def origAn = annotations.find { it == INPUT }
 		def an = new Annotation("INPUT", [
-				filename : new ConstantExpr("${name.replace ":", "_"}.facts"),
-				delimeter: new ConstantExpr("\\t")])
+				filename : origAn.args["filename"] ?: new ConstantExpr("${name.replace ":", "_"}.facts"),
+				delimeter: origAn.args["delimeter"] ?: new ConstantExpr("\\t")])
 
 		if (headElements) {
-			if (!inTypeDecl) headElements << new Relation(name, vars)
+			if (!annotations.any { it == TYPE }) headElements << new Relation(name, vars)
 			def inputRel = new Relation("__SYS_IN_$name", varN(N))
 			extraRules << new Rule(headElements.size() > 1 ? new LogicalElement(headElements) : headElements.first() as IElement, inputRel)
 			extraRelDecls << new RelDeclaration(inputRel, inputTypes, [an] as Set)
