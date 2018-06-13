7c6166cc4834853f5562f931155e9c8786f493e6
George Kastrinis <george.kast@gmail.com>
2018-06-13 14:35:48 +0300

    Move logging initialization in Main class

--- src/main/groovy/org/codesimius/panda/Main.groovy
+++ src/main/groovy/org/codesimius/panda/Main.groovy
@@ -1,6 +1,19 @@
 package org.codesimius.panda
 
+import org.apache.log4j.ConsoleAppender
+import org.apache.log4j.DailyRollingFileAppender
+import org.apache.log4j.Level
+import org.apache.log4j.Logger
+import org.apache.log4j.PatternLayout
 import org.codesimius.panda.system.Compiler
+
+def logDir = new File("build/logs")
+if (!logDir) logDir.mkdir()
+
+def root = Logger.rootLogger
+root.setLevel(Level.toLevel("INFO", Level.WARN))
+root.addAppender(new DailyRollingFileAppender(new PatternLayout("%d [%t] %-5p %c - %m%n"), "$logDir/panda.log", "'.'yyyy-MM-dd"))
+root.addAppender(new ConsoleAppender(new PatternLayout("%m%n")))
 
 println Compiler.compileToLB3(args[0], "build/out")
 println Compiler.compileToSouffle(args[0], "build/out")
--- src/main/groovy/org/codesimius/panda/system/Compiler.groovy
+++ src/main/groovy/org/codesimius/panda/system/Compiler.groovy
@@ -4,11 +4,7 @@ import org.antlr.v4.runtime.ANTLRFileStream
 import org.antlr.v4.runtime.ANTLRInputStream
 import org.antlr.v4.runtime.CommonTokenStream
 import org.antlr.v4.runtime.tree.ParseTreeWalker
-import org.apache.log4j.ConsoleAppender
-import org.apache.log4j.DailyRollingFileAppender
 import org.apache.log4j.Level
-import org.apache.log4j.Logger as Log4J
-import org.apache.log4j.PatternLayout
 import org.codesimius.panda.DatalogParserImpl
 import org.codesimius.panda.actions.code.LBCodeGenerator
 import org.codesimius.panda.actions.code.SouffleCodeGenerator
@@ -16,17 +12,6 @@ import org.codesimius.panda.datalog.DatalogLexer
 import org.codesimius.panda.datalog.DatalogParser
 
 class Compiler {
-
-	static {
-		def logDir = "./build/logs"
-		def dir = new File(logDir)
-		if (!dir) dir.mkdir()
-
-		def root = Log4J.rootLogger
-		root.setLevel(Level.toLevel("INFO", Level.WARN))
-		root.addAppender(new DailyRollingFileAppender(new PatternLayout("%d [%t] %-5p %c - %m%n"), "$logDir/panda.log", "'.'yyyy-MM-dd"))
-		root.addAppender(new ConsoleAppender(new PatternLayout("%m%n")))
-	}
 
 	static List<Result> compileToLB3(String filename, String outDir) {
 		compile(filename, new LBCodeGenerator(outDir))
@@ -37,9 +22,8 @@ class Compiler {
 	}
 
 	static List<Result> compile(String filename, def codeGenActor) {
-		Logger.log("$filename with ${codeGenActor.class.name}", "COMPILE")
-
 		try {
+			Logger.log("$filename with ${codeGenActor.class.name}", "COMPILE")
 			return compile0(new ANTLRFileStream(filename), filename, codeGenActor)
 		} catch (e) {
 			Logger.log(e.message, "ERROR", Level.ERROR, e)
